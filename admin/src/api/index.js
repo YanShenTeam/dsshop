c8f0794c5c00beb10f7e808f7b814c72a1d53787
dspurl <49799121+dspurl@users.noreply.github.com>
2021-01-14 10:35:12 +0800

    去掉安装包无用代码

--- admin/src/api/index.js
+++
@@ -1,7 +0,0 @@
-import request from '@/utils/request'
-export function indexList() {
-  return request({
-    url: 'index',
-    method: 'get'
-  })
-}
--- version/vue.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    publicPath: '/install/',
-    productionSourceMap: false,
-    lintOnSave: false
-}
